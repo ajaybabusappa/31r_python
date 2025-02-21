513d77797f23b6e5857a5b057f02ef2172d7a1ab
AJAY BABU SAPPA <jysappa@gmail.com>
2025-02-21 09:13:58 +0530

    20 Feb 2025

---
+++ 20-02.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter a number"))
+
+
+if num1 > 0:
+    print("Positive")
+elif num1 == 0:
+    print("Zero")
+else:
+    print("Negative")
+
+
+#Question2:
+if num1 % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
